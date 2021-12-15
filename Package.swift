8a4d2c62c8c29d085343c7daef8336dd7c19aaa2
jean torres <j.torres@mycoachsport.com>
2021-12-15 16:17:12 +0100

    update package

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "AlamofireObjectMapper",
     platforms: [
-            .iOS(.v12)
+        .macOS(.v10_12),.iOS(.v12)
         ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
